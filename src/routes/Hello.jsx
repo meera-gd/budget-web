0bbc43a0c8b78ba9ef94004ed763109471db3a15
Meera Godden <meera.s.gd@gmail.com>
2023-12-18 22:47:43 -0800

    Pass auth token to api

--- src/routes/Hello.jsx
+++ src/routes/Hello.jsx
@@ -1,11 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useContext } from 'react';
+
+import AuthContext from '../contexts/AuthContext';
 
 export default function Hello() {
+  const { user } = useContext(AuthContext);
+
   const { isPending, error, data } = useQuery({
     queryKey: ['greeting'],
-    queryFn: () => fetch('/api/hello/meera').then((res) => res.json()),
+    queryFn: () => user.getIdToken().then((token) => fetch('/api/hello/meera', { headers: { Authorization: `Bearer ${token}` } }).then((res) => res.json())),
   });
+
   if (isPending) return <p>Loading...</p>;
   if (error) return <p>{`Error: ${error.message}`}</p>;
   return <p>{data.message}</p>;
